22bc69dbd9806ff5de6234770fbf46f960aebed9
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-11-19 14:59:23 -0600

    made changes

--- e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
+++ e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api")
 public class ProductController {
 
