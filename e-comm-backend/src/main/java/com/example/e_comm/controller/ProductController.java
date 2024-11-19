6ca70a14c5e93a6cc4c3913b0379bdf0d9b7a644
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-11-19 00:01:20 -0600

    handled CORS and changes the mapping

--- e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
+++ e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.e_comm.controller;
 import com.example.e_comm.model.Product;
 import com.example.e_comm.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
     @Autowired
     ProductService pserv;
 
-    @GetMapping("/")
+    @GetMapping("/products")
     public List<Product> getAllProducts(){
         return pserv.getAllProducts();
     }
