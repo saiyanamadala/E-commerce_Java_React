45255e76e9e8cbdd110c1baeb0b42b4b819efd67
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-11-24 13:48:38 -0600

    added search

--- e-comm-backend/src/main/java/com/example/e_comm/repo/ProductRepo.java
+++ e-comm-backend/src/main/java/com/example/e_comm/repo/ProductRepo.java
@@ -14,7 +14,7 @@ public interface ProductRepo extends JpaRepository<Product,Integer> {
     "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "+
             "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "+
-            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))"
     )
     List<Product> searchProduct(String keyword);
 
