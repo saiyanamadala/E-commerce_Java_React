03ec45d7306688c374e82ba65f9143f8c0538123
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-11-24 11:11:42 -0600

    added Put Mapping

--- e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
+++ e-comm-backend/src/main/java/com/example/e_comm/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -55,5 +56,21 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product product1 = null;
+        try {
+            product1 = pserv.updateProduct(id,product,imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+        if(product1!=null){
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
     }
 }
